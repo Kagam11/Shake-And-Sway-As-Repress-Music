046e92446137452f81686b13a398ef8f83cf30ae
Kagam11 <hiiragi.kagami@foxmail.com>
2025-07-12 21:55:56 +0800

    update to 1.6

---
+++ Source/Shake And Sway As Repress Music/tmp.cs
@@ -0,0 +1,13 @@
+using RimWorld;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Verse;
+
+namespace ShakeAndSwayAsRepressMusic
+{
+    public class RepressTrackerComp : MapComponent
+    {
+        
+    }
+}
